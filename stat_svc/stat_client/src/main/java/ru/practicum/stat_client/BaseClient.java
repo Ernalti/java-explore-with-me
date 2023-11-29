8ee2db96d78c6fff86db737833ebfd3c492c3852
Artem Smirnov <kaban1106@yandex.ru>
2023-11-29 04:44:08 +0300

    Stat server

--- stat_svc/stat_client/src/main/java/ru/practicum/stat_client/BaseClient.java
+++ stat_svc/stat_client/src/main/java/ru/practicum/stat_client/BaseClient.java
@@ -20,12 +20,12 @@ public class BaseClient {
         return makeAndSendRequest(HttpMethod.GET, path, parameters, null);
     }
 
-    protected <T> ResponseEntity<Object> post(String path, @Nullable T body) {
+    protected <T> ResponseEntity<Object> post(String path, T body) {
         return makeAndSendRequest(HttpMethod.POST, path, null, body);
     }
 
     private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path, @Nullable Map<String, Object> parameters, @Nullable T body) {
-        HttpEntity<T> requestEntity = new HttpEntity<>(body);
+        HttpEntity<T> requestEntity =  body == null ? null : new HttpEntity<>(body);
 
         ResponseEntity<Object> shareitServerResponse;
         try {
