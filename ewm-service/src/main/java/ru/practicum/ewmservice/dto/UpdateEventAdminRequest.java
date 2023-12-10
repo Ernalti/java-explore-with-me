6b3e115e7c2bc6bcc5c34ef989f4a85b5b5b9327
Artem Smirnov <kaban1106@yandex.ru>
2023-12-11 02:16:01 +0300

    ewm-service

--- ewm-service/src/main/java/ru/practicum/ewmservice/dto/UpdateEventAdminRequest.java
+++ ewm-service/src/main/java/ru/practicum/ewmservice/dto/UpdateEventAdminRequest.java
@@ -2,12 +2,14 @@ package ru.practicum.ewmservice.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.practicum.ewmservice.model.enums.StateActionAdmin;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Data
 public class UpdateEventAdminRequest extends UpdateEventRequest {
 
 	private StateActionAdmin stateAction;
